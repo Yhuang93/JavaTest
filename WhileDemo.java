e5da4a729b75ad4f76ee24faf74d34cc46eb606d
yHuang <huangyining1993@gmail.com>
2018-05-01 23:03:04 -0700

    Hello world and while test code.

---
+++ WhileDemo.java
@@ -0,0 +1,18 @@
+package test1;
+
+public class WhileDemo{
+	
+	public static void main(String args[]){
+		int x = 10;
+		
+		int sum = 0 ;	
+		while(x>=0){
+			sum +=x ;
+			--x;
+			
+				
+		}
+		System.out.println("1 --> 10 sum=" + sum) ;
+		
+	}
+};
